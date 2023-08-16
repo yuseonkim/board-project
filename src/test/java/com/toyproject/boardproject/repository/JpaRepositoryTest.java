eed8adb25a2baae873848d7e09d41ab2bda16fb8
yuseonkim <yusun715@icloud.com>
2023-08-17 08:44:06 +0900

    Spring Data JPA 설정 변경

--- src/test/java/com/toyproject/boardproject/repository/JpaRepositoryTest.java
+++ src/test/java/com/toyproject/boardproject/repository/JpaRepositoryTest.java
@@ -8,16 +8,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
+@ActiveProfiles("testdb")
 @DisplayName("Jpa 연결 테스트")
 @Import(JpaConfig.class)
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class JpaRepositoryTest {
 
     private final ArticleRepository articleRepository;
